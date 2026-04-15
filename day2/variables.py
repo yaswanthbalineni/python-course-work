684e98e0c4b059646b166902f6bd82d7c2fbd391
Balineni yaswanth <balineniyaswanth@gmail.com>
2026-04-15 20:55:39 +0530

    Add files via upload

---
+++ day2/variables.py
@@ -0,0 +1,34 @@
+#simple program to calculate the area of rectangle
+l=20
+b=23
+area=l*b
+print("area",area)
+
+#variables
+YAS=10
+yas=20
+Yas=30
+yas_1=40
+print(YAS,Yas,yas,yas_1)
+
+#datatype
+a=10
+b=20.0
+c=3+5j
+type(a)
+type(b)
+type(c)
+print(a,b,c,type(a),type(b),type(c))
+
+ab="string"  #string
+print(ab,type(ab))
+y=[1,3,4,5,6]#list
+print(y,type(y))
+cd=(1,2,3,4,5)#tuples
+print(cd,type(cd))
+de={1,2,3,4,5}#set
+print(de,type(de))
+e={"name":"yaswanth","course":"pythonfull stack"}#dictionary
+print(e,type(e))
+
+
